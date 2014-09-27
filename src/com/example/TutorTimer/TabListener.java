ae7826193ce4a05ba027267c45f67d9e8ff196cd
Mufasa <aetherisreal42@gmail.com>
2014-09-26 23:30:48 -0700

    Add initial project files: creates two tab activity [1/1]

---
+++ src/com/example/TutorTimer/TabListener.java
@@ -0,0 +1,25 @@
+package com.example.TutorTimer;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+
+public class TabListener implements ActionBar.TabListener
+{
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft)
+    {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft)
+    {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft)
+    {
+
+    }
+}
---
+++ src/com/example/TutorTimer/TutorTimer.java
@@ -0,0 +1,31 @@
+package com.example.TutorTimer;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorTimer extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        ActionBar.Tab tab = actionBar.newTab()
+                                    .setText("Artists")
+                                    .setTabListener(new TabListener());
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                      .setText("Albums")
+                      .setTabListener(new TabListener());
+        actionBar.addTab(tab);
+    }
+}
